2f12ae3c630ecffb21813e82ada519c29e29a891
Wargen Guittap <wargenng@gmail.com>
2024-03-23 10:50:44 -0700

    introduction to javascript

---
+++ JavaScript/controlflow-functions/script.js
@@ -0,0 +1,12 @@
+let num = 1 + 1; //variable
+
+let column = ["apple", "banana", "pear"]; //array
+
+//let a = column[0];
+
+// console.log(column[1]);
+// console.log(column[2]);
+
+for (let i = 0; i < 3; i++) {
+    console.log(column[i]);
+}
